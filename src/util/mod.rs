c5ae723b01f71ab1d7114f0e53738f4b0476e2dc
aki <aki.akaguma@hotmail.com>
2024-06-19 21:27:24 +0900

    fixed clippy: `unused_imports`

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -2,4 +2,4 @@ pub mod err;
 
 mod opt_uc_x_param;
 pub use self::opt_uc_x_param::OptUcXParam;
-pub use self::opt_uc_x_param::OptUcXParamParseError;
+//pub use self::opt_uc_x_param::OptUcXParamParseError;
